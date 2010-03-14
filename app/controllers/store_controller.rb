65d5946dfc4275794d4f418208101c9d8d2ccdb6
unknown <xunglv@.(none)>
2010-03-14 15:28:27 +0700

    inital commit

---
+++ app/controllers/store_controller.rb
@@ -0,0 +1,41 @@
+class StoreController < ApplicationController
+  def index
+	  @products = Product.find_products_for_sale
+	  #@products.inspect
+  end
+	
+	def add_to_cart
+		begin
+      product = Product.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      logger.error("Attempt to access invalid product #{params[:id]}")
+      flash[:notice] = "Invalid product"
+      redirect_to :action => :index
+    else
+      #render :text => product.to_s
+      @cart  = find_cart
+      @cart.add_product(product)
+    end
+		
+	end
+
+  def empty_cart
+    session[:cart] = nil
+    flash[:notice] = "Your cart is currently empty"
+    redirect_to :action => :index
+  end
+  
+	private
+	
+	def find_cart
+		session[:cart] ||= Cart.new
+	end
+	
+	
+	
+	
+	
+  
+  
+
+end
